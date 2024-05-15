48c089db3bae1ab692cfc6733f3e7dcc651f4fea
tanvirtbf <tanver018765@gmail.com>
2024-05-15 11:28:05 +0600

    working with percentile and declare variable and print variable with percentile

--- basic program challenges/first.c
+++ basic program challenges/first.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main() {
-   printf("Hello, World!");
-   return 0;
+int main(){
+    printf("Hello World\n");
+    int age = 25;
+    printf("My age is %d\n", age);
+    char a = 'A';
+    printf("My Char is %c\n", a);
+    float f = 1.23;
+    printf("My float number is %f\n",f);
+    return 0;
 }
