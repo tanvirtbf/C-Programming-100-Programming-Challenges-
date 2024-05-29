e15ae03bff8096b236685ea0776e767b4a2efc35
tanvirtbf <tanver018765@gmail.com>
2024-05-29 15:07:27 +0600

    create our first project

---
+++ 100 + Project Folder/tanvir.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int a ;
+    printf("Please Enter a Number for A : ");
+    scanf("%d",&a);
+    printf("A is %d",a);
+
+    return 0;
+}
