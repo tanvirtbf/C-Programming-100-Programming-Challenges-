5fa3bed88be6f57d3a9a5c2d3a46c3d02142d72e
tanvirtbf <tanver018765@gmail.com>
2024-05-29 17:03:36 +0600

    area of square calculate project

--- Practice/first.c
+++ Practice/first.c
@@ -2,16 +2,11 @@
 
 int main() {
 
-    char firstName[100],lastName[100];
-    int age;
-    printf("Enter Your First Name : ");
-    scanf("%s",&firstName);
-    printf("Enter Your Last Name : ");
-    scanf("%s", &lastName);
-    printf("Enter Your Age : ");
-    scanf("%d",&age);
-
-    printf("\n\nYour Name is %s %s . And your age is %d", firstName,lastName,age);
+    int side, area;
+    printf("Enter your Side : ");
+    scanf("%d",&side);
+    area = side * side;
+    printf("Total area is : %d",area);
 
     return 0;
 }
