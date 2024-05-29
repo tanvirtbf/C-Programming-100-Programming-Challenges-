6db740a9007a02c0e9cdc9a4aeaf2cc13abb31a7
tanvirtbf <tanver018765@gmail.com>
2024-05-29 16:10:54 +0600

    sizeof operator project complete

--- Practice/first.c
+++ Practice/first.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
 int main() {
+    int a = 0;
+    float b = 123.123;
+    double c = 123.123123123;
+    char d[100] = "Hello World!";
 
-    int a = 10, b = 20;
-    printf("%d %d",a,b);
+    printf("%lu \n",sizeof(int));
+    printf("%lu \n",sizeof(float));
+    printf("%lu \n",sizeof(double));
+    printf("%lu \n",sizeof(char));
+    printf("%lu \n",sizeof(a));
+    printf("%lu \n",sizeof(b));
+    printf("%lu \n",sizeof(c));
+    printf("%lu \n",sizeof(d));
 
     return 0;
 }
