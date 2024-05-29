9876683d3ce6b0356bcab0d79fd21594226da9c4
tanvirtbf <tanver018765@gmail.com>
2024-05-29 16:25:47 +0600

    6th project done

--- Practice/first.c
+++ Practice/first.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    int a = 0;
-    float b = 123.123;
-    double c = 123.123123123;
-    char d[100] = "Hello World!";
 
-    printf("%lu \n",sizeof(int));
-    printf("%lu \n",sizeof(float));
-    printf("%lu \n",sizeof(double));
-    printf("%lu \n",sizeof(char));
-    printf("%lu \n",sizeof(a));
-    printf("%lu \n",sizeof(b));
-    printf("%lu \n",sizeof(c));
-    printf("%lu \n",sizeof(d));
+    char firstName[100],lastName[100];
+    int age;
+    printf("Enter Your First Name : ");
+    scanf("%s",&firstName);
+    printf("Enter Your Last Name : ");
+    scanf("%s", &lastName);
+    printf("Enter Your Age : ");
+    scanf("%d",&age);
+
+    printf("\n\nYour Name is %s %s . And your age is %d", firstName,lastName,age);
 
     return 0;
 }
