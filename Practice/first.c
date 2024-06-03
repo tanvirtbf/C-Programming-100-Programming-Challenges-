8e14d05b69cb4004ee3c3dca5fdf7a5e86a1d72a
tanvirtbf <tanver018765@gmail.com>
2024-06-03 12:15:27 +0600

    declare some valid variable

--- Practice/first.c
+++ Practice/first.c
@@ -2,8 +2,14 @@
 
 int main() {
 
-    printf("Hello");
-
+    int age;
+    int i = 10, j=25;
+    float temp = 98.4;
+    float a = 1.4, b=1.44 + 2.43 * 1.111;
+    float p = 4, q = p*2;
+    char names[50];
+    int x,y,z=5;
+    printf("%d",j);
 
     return 0;
 }
