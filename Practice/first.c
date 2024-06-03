58aec4c959e0b71badbd8d23003f37c212924d6b
tanvirtbf <tanver018765@gmail.com>
2024-06-03 12:06:58 +0600

    start with C instractions

--- Practice/first.c
+++ Practice/first.c
@@ -2,11 +2,8 @@
 
 int main() {
 
-    int side, area;
-    printf("Enter your Side : ");
-    scanf("%d",&side);
-    area = side * side;
-    printf("Total area is : %d",area);
+    int a,b;
+
 
     return 0;
 }
