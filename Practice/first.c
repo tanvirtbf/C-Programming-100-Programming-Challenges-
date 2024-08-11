7483cb32332ea6a27cd25036ff8df85a3ea33cce
tanvirtbf <tanver018765@gmail.com>
2024-08-11 16:27:05 +0600

    again start

--- Practice/first.c
+++ Practice/first.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
 
 int main() {
-
-    int age;
-    int i = 10, j=25;
-    float temp = 98.4;
-    float a = 1.4, b=1.44 + 2.43 * 1.111;
-    float p = 4, q = p*2;
-    char names[50];
-    int x,y,z=5;
-    printf("%d",j);
-
+    printf("My age is %d",23);
     return 0;
 }
