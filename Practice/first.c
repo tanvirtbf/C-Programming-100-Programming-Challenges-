ac0d808672f995da354724ced5bb970ad9e7fa0c
tanvirtbf <tanver018765@gmail.com>
2024-08-12 10:50:07 +0600

    some of two numbers problem solved

--- Practice/first.c
+++ Practice/first.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-
+// sum of two numbers
 int main() {
-    int age;
-    printf("Enter your age : \n");
-    scanf("%d",&age);
-    printf("Your age is %d",age);
+    int num1, num2, num3;
+    printf("Please Enter your 3 numbers : ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+    const int sum = num1 + num2 + num3;
+    printf("Sum : %d", sum);
     return 0;
 }
