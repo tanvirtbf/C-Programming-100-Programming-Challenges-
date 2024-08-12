e0b38e95d6eb38ac2fcf4c2d382d1106cd518ec5
tanvirtbf <tanver018765@gmail.com>
2024-08-12 11:59:01 +0600

    update

--- Practice/first.c
+++ Practice/first.c
@@ -1,10 +1,3 @@
 #include <stdio.h>
-// sum of two numbers
 int main() {
-    int num1, num2, num3;
-    printf("Please Enter your 3 numbers : ");
-    scanf("%d %d %d", &num1, &num2, &num3);
-    const int sum = num1 + num2 + num3;
-    printf("Sum : %d", sum);
-    return 0;
 }
