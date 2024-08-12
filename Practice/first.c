99808924b80808687b9d29a137390e686b69ae70
tanvirtbf <tanver018765@gmail.com>
2024-08-12 10:28:35 +0600

    use const keyword for constant variable

--- Practice/first.c
+++ Practice/first.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int a = 5;
-    float out;
-    printf("%d",a);
-    a = 10;
-    printf("%d",a);
+    const float PI = 3.1416;
+    printf("%f",PI);
     return 0;
 }
