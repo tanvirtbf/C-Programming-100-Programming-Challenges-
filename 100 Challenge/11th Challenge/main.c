1dc64489305d5e0c38a9b17553cee69c0e96b478
tanvirtbf <tanver018765@gmail.com>
2024-08-18 22:41:05 +0600

    12th problem solve

--- 100 Challenge/11th Challenge/main.c
+++ 100 Challenge/11th Challenge/main.c
@@ -6,6 +6,7 @@ int main(){
     printf("Enter two float number : \n");
     scanf("%f%f",&float1,&float2);
     printf("%f * %f = %f", float1, float2, (float1*float2));
+    printf("%f * %f = %.3f", float1, float2, (float1*float2)); // point er por 3 ta number ashbe
 
     return 0;
 }
--- 100 Challenge/12th Challenge/main.c
+++ 100 Challenge/12th Challenge/main.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
-// Create a program that takes two numbers and shows result of all arithmetic operators(+,-,*,/,%)
+// Create a program that determines if a number is positive, negative or Zero
 
 int main(){
-    int a,b;
-    printf("Enter Two number : ");
-    scanf("%d%d",&a,&b);
-    printf("%d + %d = %d\n", a, b, (a+b));
-    printf("%d - %d = %d\n", a, b, (a-b));
-    printf("%d * %d = %d\n", a, b, (a*b));
-    printf("%d / %d = %d\n", a, b, (a/b));
-    printf("%d mod %d = %d\n", a, b, (a%b));
+    int num;
+    printf("Enter a Number : ");
+    scanf("%d",&num);
+    if(num==0){
+        printf("%d is zero\n",num);
+    }else if(num>0){
+        printf("%d is positive\n",num);
+    }else if(num<0){
+        printf("%d is negative\n",num);
+    }
 
     return 0;
 }
