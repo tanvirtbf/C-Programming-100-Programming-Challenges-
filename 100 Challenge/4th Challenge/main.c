93da319194010e71e6934d91816c9859cdbef5a5
tanvirtbf <tanver018765@gmail.com>
2024-08-18 16:09:46 +0600

    4th problem solved

---
+++ 100 Challenge/4th Challenge/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// Create a Program to declare two integer variables, assign then values and display their values
+int main(){
+    int first,second;
+    printf("Please Enter First Number : ");
+    scanf("%d",&first);
+    printf("Please Enter Second Number : ");
+    scanf("%d",&second);
+
+    printf("First number is : %d and Second number is : %d", first,second);
+    return 0;
+}
