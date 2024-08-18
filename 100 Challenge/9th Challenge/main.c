06a99c3e2fdbf1ec3308bfb489781ed02a5a52f4
tanvirtbf <tanver018765@gmail.com>
2024-08-18 21:17:15 +0600

    9th problem solved

---
+++ 100 Challenge/9th Challenge/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+// Create a program that takes two numbers and shows result of all arithmetic operators(+,-,*,/,%)
+
+int main(){
+    int a,b;
+    printf("Enter Two number : ");
+    scanf("%d%d",&a,&b);
+    printf("Addition Value is : %d\n", a+b);
+    printf("Subtraction Value is : %d\n", a-b);
+    printf("Multiplication Value is : %d\n", a*b);
+    printf("Division Value is : %d\n",a/b);
+    printf("Modulas Value is : %d\n", a%b);
+    return 0;
+}
