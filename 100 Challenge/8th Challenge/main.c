14968d96082f4e09bfa5e315bb64185b94471cf6
tanvirtbf <tanver018765@gmail.com>
2024-08-18 21:09:23 +0600

    8th problem solved

--- 100 Challenge/8th Challenge/main.c
+++ 100 Challenge/8th Challenge/main.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
-// Create a program to print the area of a square by inputting its side length
+// Create a program to swap two numbers
 int main(){
-    int side;
-    printf("Please Enter the side of square in cms : ");
-    scanf("%d",&side);
-    printf("The area of your square is %d cm*cm", side*side);
+
+    int firstNumber, secondNumber, temp;
+
+    printf("Enter the first number : ");
+    scanf("%d",&firstNumber);
+    printf("Enter the second number : ");
+    scanf("%d",&secondNumber);
+
+    temp = firstNumber;
+    firstNumber = secondNumber;
+    secondNumber = temp;
+
+    printf("First Number is %d and Second Number is %d",firstNumber,secondNumber);
+
     return 0;
 }
