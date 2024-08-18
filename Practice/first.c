d4d4afac0bc2720b71de343a8f646a3e166f39e1
tanvirtbf <tanver018765@gmail.com>
2024-08-18 15:06:41 +0600

    A mini project with ternary operator and goto statement

--- Practice/first.c
+++ Practice/first.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
-// Decision Control Structure
-int main() {
 
+int main() {
+    int myAge = 25;
     int age;
-    input_number:
-    printf("Please Enter age : ");
+    int x = 0;
+    again_age:
+    x++;
+    x==1 ? printf("Please Enter Your Age : ") : x==2 ? printf("Your Answer is Wrong! Second time Please Enter Your Age : ") : x == 3 ? printf("Your Answer is wrong! Third time Please Enter Your Age : ") : printf("Your Answer is Wrong! Fourth and Last time Please Enter Your Age : ");
     scanf("%d",&age);
-    if(age != 25){
-        goto input_number;
+    if(age != myAge && x<4){
+        goto again_age;
     }
     return 0;
 }
