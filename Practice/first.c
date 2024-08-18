013aff0cc5f3cf13ff82a91c7f5e39cc9054f8cf
tanvirtbf <tanver018765@gmail.com>
2024-08-18 14:01:44 +0600

    ternary operator

--- Practice/first.c
+++ Practice/first.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 // Decision Control Structure
 int main() {
-
+    char gender;
+    printf("Please Enter Your Gender ? (M/F) : ");
+    scanf(" %c",&gender);
+    gender == 'M' ? printf("Male") : gender == 'm' ? printf("male") : gender == 'f' ? printf("female") : gender == 'F' ? printf("Female") :printf("Wrong Input");
     return 0;
 }
