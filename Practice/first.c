25d3707ab5f931f51fe0e1e3c097d5aed6fc48c7
tanvirtbf <tanver018765@gmail.com>
2024-08-18 14:49:52 +0600

    goto start

--- Practice/first.c
+++ Practice/first.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 // Decision Control Structure
 int main() {
-    char gender;
-    printf("Please Enter Your Gender ? (M/F) : ");
-    scanf(" %c",&gender);
-    gender == 'M' ? printf("Male") : gender == 'm' ? printf("male") : gender == 'f' ? printf("female") : gender == 'F' ? printf("Female") :printf("Wrong Input");
+
+    int age;
+    printf("Please Enter age : ");
+    scanf("%d",&age);
+    switch(age){
+    case 18 :
+        printf("Matured\n");
+        break;
+    case 25 :
+        printf("Biyer boyosh hoise\n");
+        break;
+    case 32 :
+        printf("fully matured\n");
+        break;
+    case 40 :
+        printf("intermediate age\n");
+        break;
+    case 60 :
+        printf("Bura hoise\n");
+        break;
+    default :
+        printf("Nothing");
+        break;
+    }
     return 0;
+
 }
