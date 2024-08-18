201c3669328a1d853eecc5a5c574d7390507ea0f
tanvirtbf <tanver018765@gmail.com>
2024-08-18 14:54:36 +0600

    goto example

--- Practice/first.c
+++ Practice/first.c
@@ -3,28 +3,11 @@
 int main() {
 
     int age;
+    input_number:
     printf("Please Enter age : ");
     scanf("%d",&age);
-    switch(age){
-    case 18 :
-        printf("Matured\n");
-        break;
-    case 25 :
-        printf("Biyer boyosh hoise\n");
-        break;
-    case 32 :
-        printf("fully matured\n");
-        break;
-    case 40 :
-        printf("intermediate age\n");
-        break;
-    case 60 :
-        printf("Bura hoise\n");
-        break;
-    default :
-        printf("Nothing");
-        break;
+    if(age != 25){
+        goto input_number;
     }
     return 0;
-
 }
