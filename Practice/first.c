5eb5ccedc81fe76cf346f2e4aa971d8e79e63012
tanvirtbf <tanver018765@gmail.com>
2024-08-19 09:51:15 +0600

    function and recursion start

--- Practice/first.c
+++ Practice/first.c
@@ -1,15 +1,5 @@
 #include <stdio.h>
-
+// function and recursion
 int main() {
-    int myAge = 25;
-    int age;
-    int x = 0;
-    again_age:
-    x++;
-    x==1 ? printf("Please Enter Your Age : ") : x==2 ? printf("Your Answer is Wrong! Second time Please Enter Your Age : ") : x == 3 ? printf("Your Answer is wrong! Third time Please Enter Your Age : ") : printf("Your Answer is Wrong! Fourth and Last time Please Enter Your Age : ");
-    scanf("%d",&age);
-    if(age != myAge && x<4){
-        goto again_age;
-    }
-    return 0;
+
 }
