069c8ba359cbc64eb82dd55d8cd3e836f9270960
tanvirtbf <tanver018765@gmail.com>
2024-08-20 12:46:26 +0600

    26th problem solve

--- 100 Challenge/26th Challenge/main.c
+++ 100 Challenge/26th Challenge/main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-// Create a program to check whether a given number is prime using while.
+// Create a program to find the Least Common Multiple(LCM) of two numbers
 
 int main(){
-    int x;
-    printf("Enter a Number for check if Prime or not : ");
-    scanf("%d",&x);
-    for(int i = 2; i<x; i++){
-        if(x%i==0){
-            printf("%d is not a prime number\n",x);
+    int x,y;
+    printf("Enter two Numbers : \n");
+    scanf("%d%d",&x,&y);
+    int minimumInt = x<y ? x : y;
+    for(int i = minimumInt; i<=x*y; i++){
+        if(i%x==0 && i%y==0){
+            printf("%d is LCM",i);
             return 0;
         }
     }
-    printf("%d is a Prime Number \n",x);
-    return 0;
+
 }
