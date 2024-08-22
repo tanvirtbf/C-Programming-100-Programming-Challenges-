4f5af3dd83b9a8efab5e8dfa880c3a05d69d4c58
tanvirtbf <tanver018765@gmail.com>
2024-08-22 18:03:07 +0600

    update

--- 100 Challenge/29th Challenge/main.c
+++ 100 Challenge/29th Challenge/main.c
@@ -5,9 +5,37 @@ int main(){
     int num;
     printf("Enter a Number : ");
     scanf("%d",&num);
-    printf("%d",num);
+    printf("%d",0);
     if(num>0){
         printf("%d",1);
     }
+    int prev = 0;
+    int next = 1;
+    while(prev+next <= num){
+        int temp = prev+next;
+        printf("%d",temp);
+        prev = next;
+        next = temp;
+    }
     return 0;
 }
+
+/*
+
+int main(){
+    int x, result=0, prev;
+    printf("Enter a Number : ");
+    scanf("%d",&x);
+    printf("%d\n",result);
+    prev = result; // 0
+    result = 1;
+    while(result<x){
+        printf("%d\n",result); // 0 1 1 2 3 5 8
+        int now = result; // 5
+        result = result + prev; // 8
+        prev = now; // 5
+    }
+    return 0;
+}
+
+*/
