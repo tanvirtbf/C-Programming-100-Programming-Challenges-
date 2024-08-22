1fd031ac69e239532807598f23a8d7af01f158cd
tanvirtbf <tanver018765@gmail.com>
2024-08-22 17:58:11 +0600

    complete 29 th problem

--- 100 Challenge/29th Challenge/main.c
+++ 100 Challenge/29th Challenge/main.c
@@ -2,11 +2,17 @@
 // Create a program to print the Fibonacci series up to a certain number
 
 int main(){
-    int x, result=0;
+    int x, result=0, prev;
     printf("Enter a Number : ");
     scanf("%d",&x);
+    printf("%d\n",result);
+    prev = result; // 0
+    result = 1;
     while(result<x){
-
+        printf("%d\n",result); // 0 1 1 2 3 5 8
+        int now = result; // 5
+        result = result + prev; // 8
+        prev = now; // 5
     }
     return 0;
 }
