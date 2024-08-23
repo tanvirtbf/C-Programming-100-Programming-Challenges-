c93fbf8b216ef82312ea6e011da51f1e2cff2f3a
tanvirtbf <tanver018765@gmail.com>
2024-08-23 19:57:18 +0600

    35th problem solve

--- 100 Challenge/35th Challenge/main.c
+++ 100 Challenge/35th Challenge/main.c
@@ -2,7 +2,11 @@
 // Create a program using for loop multiplication table for a number
 
 int main(){
-
-
+    int num;
+    printf("Enter a Number : ");
+    scanf("%d",&num);
+    for(int i = 1; i<=10; i++){
+        printf("%d X %d = %d\n", num, i , (num*i));
+    }
     return 0;
 }
