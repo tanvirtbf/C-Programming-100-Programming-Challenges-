6e1b76c82f42d738abf21bb539608a225d7dc54d
tanvirtbf <tanver018765@gmail.com>
2024-08-23 19:53:35 +0600

    update

--- 100 Challenge/35th Challenge/main.c
+++ 100 Challenge/35th Challenge/main.c
@@ -1,19 +1,8 @@
 #include <stdio.h>
-// Create a program to verify if a user put a positive number
+// Create a program using for loop multiplication table for a number
 
 int main(){
-    int x = 0;
-    do{
-        int num ;
-        x==0? printf("Enter a Number : \n"): printf("Please Enter a Positive Number : \n");
-        scanf("%d",&num);
-        if(num<0){
-            x = 1;
-        }else{
-            printf("Thanks for putting a Positive Number \n");
-            return 0;
-        }
-    }while(x==1);
+
 
     return 0;
 }
