4409fddc3ef6adac05816184702834efc6423637
tanvirtbf <tanver018765@gmail.com>
2024-08-23 20:19:26 +0600

    update

--- 100 Challenge/36th Challenge/main.c
+++ 100 Challenge/36th Challenge/main.c
@@ -2,18 +2,5 @@
 // Create a program using continue to sum all positive numbers entered by the user; skip any negative numbers and out for enter 0.
 
 int main(){
-    int num , sum=0;
-    again:
-    printf("Enter a number : ");
-    scanf("%d",&num);
-    if(num>0){
-        sum += num;
-        goto again;
-    }else if(num<0){
-        goto again;
-    }else if(num==0){
-        printf("%d",sum);
-        return 0;
-    }
-    return 0;
+
 }
