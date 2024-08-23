8e625b6f2b2065d177d62c3e344b10730faf0265
tanvirtbf <tanver018765@gmail.com>
2024-08-23 19:49:17 +0600

    34th problem solve

--- 100 Challenge/34th Challenge/main.c
+++ 100 Challenge/34th Challenge/main.c
@@ -2,18 +2,13 @@
 // Create a program to verify if a user put a positive number
 
 int main(){
-    int x = 0;
+    int num,sum = 0;
     do{
-        int num ;
-        x==0? printf("Enter a Number : \n"): printf("Please Enter a Positive Number : \n");
-        scanf("%d",&num);
-        if(num<0){
-            x = 1;
-        }else{
-            printf("Thanks for putting a Positive Number \n");
-            return 0;
-        }
-    }while(x==1);
-
+       printf("Enter a Number : ");
+       scanf("%d",&num);
+       sum += num;
+    }
+    while(num!=0);
+    printf("%d",sum);
     return 0;
 }
