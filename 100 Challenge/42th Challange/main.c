9de2844076c3cafc2f5f5ce75da6ee851a785c2b
tanvirtbf <tanver018765@gmail.com>
2024-08-24 10:19:26 +0600

    42th problem solve

--- 100 Challenge/42th Challange/main.c
+++ 100 Challenge/42th Challange/main.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
-// write a function named greet that prints "Hello, World!" when called
+// create a function max that takes two float arguments and returns the larger value
 
-void greet(); // function prototype
+float maxVal(float,float); // function prototype
 
 int main(){
-    greet(); // function call
+    float x,y;
+    printf("Enter two float number :\n");
+    hello:
+    scanf("%f%f",&x,&y);
+    float result = maxVal(x,y); // function call
+    if(result==10.000000){
+        printf("Please do not input equal value\nEnter again: \n");
+        goto hello;
+    }
+    printf("%f is max value", result);
     return 0;
 }
 
 // function definition
-void greet(){
-    printf("Hello, World!");
+float maxVal(float a, float b){
+    if(a>b){
+        return a;
+    }else if(a<b){
+        return b;
+    }else{
+        return 10;
+    }
 }
